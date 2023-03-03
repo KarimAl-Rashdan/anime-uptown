110ac41bf19ed7121adbe0b5acd9e2a1ea59359d
karimy <karim.alrashdan14@gmail.com>
2023-03-02 17:03:15 -0700

    Create state for anime titles

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,6 +1,13 @@
 import "../MainPage/MainPage.css"
+import React, { useState, useEffect } from "react"
+
 
 function MainPage() {
+  const [animeTitles, setAnimeTitles] = useState([])
+
+  useEffect(() => {
+    
+  })
   return (
     <main className="mainpage">
       <h2>hey</h2>

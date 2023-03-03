198d00a0268891fa2056d2e60fec07feb0b6c95c
karimy <karim.alrashdan14@gmail.com>
2023-03-03 12:07:02 -0700

    Add BrowserRouter to index file

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,8 +1,11 @@
 import "../NavBar/NavBar.css"
+import React from "react";
+import {Link} from "react-router-dom"
 
 function NavBar() {
  return (
   <div className="navbarContainer">
+    <Link to={`/myanimelist`}>MyAnimeList</Link>
     <p>hi</p>
     <p>hello</p>
     <p>how u doing</p>

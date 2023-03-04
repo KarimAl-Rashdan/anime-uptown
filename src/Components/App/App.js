9e4667c14e126a04082eb938cc689f14f3156e13
karimy <karim.alrashdan14@gmail.com>
2023-03-03 17:21:48 -0700

    Add styling to Nav button

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -19,7 +19,7 @@ function App() {
       <Link to="/">logo</Link>
       <div className="navBarWrapper">
         {navBar && <NavBar />}
-        <button onClick={() => toggleNavBar()}>Nav</button>
+        <button onClick={() => toggleNavBar()} className="navBtn">Nav</button>
       </div>
       <Switch>
         <Route

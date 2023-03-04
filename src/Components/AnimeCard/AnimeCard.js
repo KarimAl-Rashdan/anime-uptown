68937d2168e82192ad1e41d54784f4d547e95545
karimy <karim.alrashdan14@gmail.com>
2023-03-04 03:10:50 -0700

    Add unsaveicon and styling to AnimeCard component

--- src/Components/AnimeCard/AnimeCard.js
+++ src/Components/AnimeCard/AnimeCard.js
@@ -1,10 +1,14 @@
 import "../AnimeCard/AnimeCard.css"
 import React from "react";
+import saveIcon from "../../images/saveicon.png"
+import unSaveIcon from "../../images/unsaveicon.png"
 
 function AnimeCard({image, title, rating}) {
 return (
   <div className="animeCard">
-    
+    <div className="iconContainer">
+      <img src={unSaveIcon} alt="Not checked save icon"></img>
+    </div>
     <img src={image} alt={title}></img>
     <p>{title}</p>
     <p>{rating} Rating</p>

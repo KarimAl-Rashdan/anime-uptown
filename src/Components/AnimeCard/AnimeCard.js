14bb085ff0c236a545861702509958cf7daac665
karimy <karim.alrashdan14@gmail.com>
2023-03-04 03:24:45 -0700

    Create toggleSaveIcon function

--- src/Components/AnimeCard/AnimeCard.js
+++ src/Components/AnimeCard/AnimeCard.js
@@ -1,13 +1,20 @@
 import "../AnimeCard/AnimeCard.css"
-import React from "react";
-import saveIcon from "../../images/saveicon.png"
+import React, { useState } from "react";
+import saveOrangeIcon from "../../images/saveicon.png"
 import unSaveIcon from "../../images/unsaveicon.png"
 
 function AnimeCard({image, title, rating}) {
+  const [saveIcon, setSaveIcon] = useState(false)
+
+  const toggleSaveIcon = () => {
+    setSaveIcon(!saveIcon)
+  }
 return (
   <div className="animeCard">
     <div className="iconContainer">
-      <img src={unSaveIcon} alt="Not checked save icon"></img>
+      <button onClick={() => toggleSaveIcon()}>
+        {saveIcon ? <img src={saveOrangeIcon} alt="Checked save icon"></img> : <img src={unSaveIcon} alt="Not checked save icon"></img>}
+      </button>
     </div>
     <img src={image} alt={title}></img>
     <p>{title}</p>
